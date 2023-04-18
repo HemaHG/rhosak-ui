cfdbcca6cbe51719b7364e66a6850cc5b0a7a60d
hemahg <hhg@redhat.com>
2023-04-18 13:49:24 +0530

    chore: Remove kafka overview page
    Signed-off-by: hemahg <hhg@redhat.com>

--- apps/consoledot-rhosak/src/routes/Routes.tsx
+++ apps/consoledot-rhosak/src/routes/Routes.tsx
@@ -2,14 +2,10 @@ import type { VoidFunctionComponent } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { ControlPlaneRoutes, DrawerProvider } from "./control-plane";
 import { DataPlaneRoutes } from "./data-plane";
-import { OverviewRoute } from "./overview";
 
 export const Routes: VoidFunctionComponent = () => {
   return (
     <Switch>
-      <Route path={"/overview"} exact>
-        <OverviewRoute />
-      </Route>
       <Redirect from={"/"} to={"/kafkas"} exact />
       <Route path={"/kafkas"}>
         <DrawerProvider>
